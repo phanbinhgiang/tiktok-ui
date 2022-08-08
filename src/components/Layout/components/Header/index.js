d8425ab4aaef08af6072e655c07153ad7f5206ab
Giang Phan <phanbinhgiang1999@gmail.com>
2022-08-08 22:40:50 +0700

    update

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons'
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css'; // optional
 
@@ -10,8 +10,24 @@ import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles)
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: "English",
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: "Feedback and help",
+        to: "/feedback"
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: "Keyboard shortcuts",
+    },
+]
 function Header() {
     const [searchResults, setSearchResults] = useState([])
     useEffect(() => {
@@ -52,8 +68,6 @@ function Header() {
                         </button>
                         <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
-
-
                         <button className={cx('search-btn')} >
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
@@ -64,6 +78,14 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
+
+                    <Menu
+                        items={MENU_ITEMS}
+                    >
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
 
---
+++ src/components/Popper/Menu/index.js
@@ -0,0 +1,35 @@
+import classNames from "classnames/bind";
+import Tippy from "@tippyjs/react/headless";
+import { Wrapper as PopperWrapper } from "~/components/Popper"
+
+import styles from "./Menu.module.scss"
+import MenuItem from "./MenuItem"
+const cx = classNames.bind(styles)
+
+function Menu({ children, items = [] }) {
+    const renderItems = () => {
+        return items.map((item, index) => (
+            <MenuItem key={index} data={item} />
+        ))
+    }
+
+    return (
+        <Tippy
+            interactive
+            placement='bottom-end'
+            render={attrs => (
+                <div className={cx('content')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper>
+                        {renderItems()}
+                    </PopperWrapper>
+                </div>
+            )
+            }
+        >
+            {children}
+
+        </Tippy>
+    );
+}
+
+export default Menu;
