dff77b2e1a03bb11a4b0b1ba9165ff587cfc657a
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-08-17 15:26:14 +0700

    Enhance Code #1

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -21,7 +21,7 @@ import Menu from '~/components/Popper/Menu';
 import Image from '~/components/Image';
 import { MessageIcon, UploadIcon, InboxIcon } from '~/components/Icons';
 import Search from '../Search';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 const MENU_ITEMS = [
@@ -94,7 +94,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="TikTok" />
                 </Link>
                 <Search />
